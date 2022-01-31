a5a9b95bd5599178169ec2c39f297962eafd9936
Philipp <Philipp1990@web.de>
2022-01-31 20:40:15 +0100

    - Add option Route

--- config/routes.php
+++ config/routes.php
@@ -137,6 +137,7 @@ $routes->scope('/api/v1/',['controller' => 'magazines'],function (RouteBuilder $
     $builder->get('magazines/list',['action' => 'getMagazineList']);
     $builder->options('magazines/list',['action' => 'optionsRequest']);
     $builder->put('magazines/{id}',['action' => 'updateMagazineData']);
+    $builder->options('magazines/index',['action' => 'optionsRequest']);
     $builder->post('magazines/index',['action' => 'indexMagazines']);
 });
 $routes->scope('/api/v1/',['controller' => 'wikiPages'],function (RouteBuilder $builder){
